51b5cd17a9d86440c25d6f381c828892072aa4e0
Fe3o4 <sudi141@163.com>
2016-08-19 04:51:16 +0800

    add transition
    add transtition

--- src/app.vue
+++ src/app.vue
@@ -5,22 +5,21 @@
 				router-link(to='/repo') repo
 				router-link(to='/org') org
 				a.pull-right(href='https://github.com/Fantasy15/github-search-1.0') built with vue.js
-		.container-sm
-			router-view
+		transition(name='fade',mode='out-in')
+			router-view.container-sm
 </template>
-<style>
+<style lang="sass">
 	body{
 		margin: 0;
 		font-size: 14px;
 		font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
 		background-color: #f5f5f5;
 	}
+	*{
+		box-sizing: border-box;
+	}
 	a{
 		color:#337ab7;
-		text-decoration: none;
-	}
-	a:focus,a:hover{
-		color: #23527c;
 		text-decoration: none;
 	}
 	.clearfix:after{
@@ -30,29 +29,45 @@
 		overflow:hidden;
 	}
 	.header{
+		position:fixed;
+		top: 0;
+		left: 0;
+		right: 0;
+		z-index: 999;
 		background-color: #42b782;
 		color: #fff;
-		margin-bottom: 20px;
-	}
-	.navbar{
-		max-width: 960px;
-		margin: 0 auto;
-	}
-	.navbar a{
-		display: inline-block;
-		font-size: 16px;
-		line-height: 20px;
-		padding: 15px 15px;
-		color: #fff;
-	}
-	.navbar .pull-right{
-		float: right;
+		.navbar{
+			max-width: 960px;
+			margin: 0 auto;
+			a{
+				display: inline-block;
+				padding: 15px 15px;
+				font-size: 16px;
+				line-height: 20px;
+				color: rgba(255,255,255,.8);
+				transition: color .2s ease ;
+				&:hover,&.router-link-active{
+					color:#fff;
+				}
+			}
+			.pull-right{
+				float: right;
+			}
+		}
 	}
 	.container-sm{
 		max-width: 960px;
-		margin: 0 auto;
+		margin: 80px auto 0;
+		padding:  15px 24px 60px;
+		background-color: #fff;
 	}
-	[v-cloak]{
-		display: none
+	.fade-enter-active{
+	  transition: opacity .3s
+	}
+	.fade-leave-active{
+	  transition: opacity .5s
+	}
+	.fade-leave-active,.fade-enter{
+	  opacity: 0
 	}
 </style>
