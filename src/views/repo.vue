be91653cd8d50d3a692f8d063973cf479ed479f3
Fe3o4 <sudi141@163.com>
2016-08-19 04:54:02 +0800

    update

--- src/views/repo.vue
+++ src/views/repo.vue
@@ -1,7 +1,9 @@
 <template lang="pug">
-	.repoContainer
+	div
+		loading( :show="loading")
 		search(ref='search',@query='load')
-		repo-list( :repo-list='repoList', :org-shiw='0')
+		transition( :name='transition')
+			repo-list.list( :repo-list='repoList', :org-show='0')
 		page-bar(ref='page',@query='load')
 </template>
 
@@ -10,34 +12,44 @@
 		name: 'repo',
 		data () {
 			return {
+				transition: 'slide-left',
+				loading: false,
 				per_page:6
 			}
 		},
-		computed: Vuex.mapGetters(['repoList']),
+		computed: Vuex.mapGetters(['repoList','total']),
 		methods:{
 			load () {
+				this.loading = true
 				const params = {
 					query: this.$refs.search.content,
 					page: this.$refs.page.page,
 					per_page: this.per_page
-			}
-				return this.$store.dispatch('loadRepo',params)
+				}
+				this.$store.dispatch('loadRepo',params).then(() => {
+					this.transition = 'slide-right'
+					this.loading = false
+				})
 			}
 		},
 		mounted () {
 			this.load();
 		},
 		components:{
+			loading: require("../components/loading.vue"),
 			search: require("../components/search.vue"),
 			repoList: require("../components/repoList.vue"),
 			pageBar: require("../components/pageBar.vue")
 		}
 	}
-
 </script>
-<style scope>
-	.repoContainer{
-		background-color: #fff;
-		padding: 15px 24px 60px;
+<style lang="sass">
+	.slide-right-leave-active,.slide-left-enter{
+		opacity:0;
+		transform: translate(140px 0);
+	}
+	.slide-left-leave-active, .slide-right-enter{
+		opacity:0;
+		transform: translate(-140px, 0);
 	}
 </style>
