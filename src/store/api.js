c0dbe4beba72634356f36fb65879931267713f1e
Fe3o4 <sudi141@163.com>
2016-08-20 01:11:21 +0800

    fetch instead of $.ajax

--- src/store/api.js
+++ src/store/api.js
@@ -1,47 +1,22 @@
-import {ajax} from 'jquery'
-
 export default {
+	//ugly writing and rewrite later
 	loadRepo ({query,page,per_page}) {
-		const sendJSON = {q:(query.length>0?query+"\+":"")+"stars:>=0",sort:"stars",page,per_page};
-		if(query.length>0){
-			delete(sendJSON.sort)
-		}
-		return new Promise((resolve,reject) => {
-			ajax({
-	  			url: 'https://api.github.com/search/repositories',
-	          	type: "GET",
-	          	contentType: "application/json; charset=utf-8",
-	        	data: sendJSON,
-	        })
-	        .done(function(data) {
-	          	resolve(data)
-	        })
-	        .fail(function(e) {
-	          	console.log(e)
-	        })
-		})
+		let baseUrl = "https://api.github.com/search/repositories?q="
+			+(query.length>0?query+"+stars:>=0":"stars:>=0&sort=stars")
+			+"&page="+page
+			+"&per_page="+per_page
+		return fetch(baseUrl).then(res => res.json())
+  			.then(data => data)
+  			.catch(e => console.log("uh error", e))
 	},
 	loadRepoByOrg ({org,query,page,per_page}) {
-		let sendJSON;
-		if(page==1 && query.length>0){
-			sendJSON = {query}
-		}else{
-			sendJSON = {query,page,per_page}
+		let baseUrl = "https://api.github.com/orgs/"+org+"/repos?query="+query;
+		if(!(page==1 && query.length>0)){
+			baseUrl = baseUrl+"&page="+page+"&per_page="+per_page
 		}
-		return new Promise( (resolve,reject) => {
-			ajax({
-	  			url: 'https://api.github.com/orgs/'+org+'/repos',
-	          	type: "GET",
-	          	contentType: "application/json; charset=utf-8",
-            	data: sendJSON,
-	        })
-	        .done(function(data) {
-	          	resolve(data)
-	        })
-	        .fail(function(e) {
-	          	console.log(e)
-	        })
-		})
+		return fetch(baseUrl).then(res => res.json())
+			.then(data => data)
+			.catch(e => console.log("uh error", e))
 	}
 }
 
