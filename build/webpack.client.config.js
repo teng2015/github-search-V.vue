9479be91e829fd466f76eb29a5ffd84e2d997f6f
Fe3o4 <sudi141@163.com>
2016-08-20 12:31:09 +0800

    remove log

--- build/webpack.client.config.js
+++ build/webpack.client.config.js
@@ -2,10 +2,7 @@ var config = require('./webpack.base.config')
 var webpack = require('webpack')
 var openBrowser = require('open-browser-webpack-plugin');
 
-// here is a bug that just can't enter pro even you set NODE_ENV=production in package.json
-// later to fix
 if (process.env.NODE_ENV == 'production') {
-  console.log("enter pro build")
   var ExtractTextPlugin = require('extract-text-webpack-plugin')
 
   config.plugins.push(
@@ -16,7 +13,6 @@ if (process.env.NODE_ENV == 'production') {
     })
   )
 }else{
-  console.log("enter dev build")
   config.plugins.push(
     new openBrowser({
       url: 'http://localhost:8080',
